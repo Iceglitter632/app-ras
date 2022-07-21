2d1ccee526eb757736d7d517a5807867b7b909ca
Iceglitter632 <52899186+Iceglitter632@users.noreply.github.com>
2022-07-21 14:51:20 +0200

    update utils.py

--- utils.py
+++ utils.py
@@ -28,6 +28,25 @@ def rgba2gray(rgba):
     return gray
 
 def storetable(filename, dict_):
+
+    t1 = dict_['imu']
+    t2 = dict_['speedometer']
+    t2 = np.array(t2)
+    t2 = np.expand_dims(t2, axis=1)
+    t3 = dict_['command']
+    t3 = np.array(t3)
+    t3 = np.expand_dims(t3, axis=1)
+    t4 = dict_['labels']
+    
+    arr = np.concatenate([t1, t2, t3, t4], axis=1)
+    
+    del dict_['imu']
+    del dict_['speedometer']
+    del dict_['command']
+    del dict_['labels']
+    
+    dict_['others'] = arr
+    
     with h5py.File(filename, "w") as file:
         for k, v in dict_.items():
             file.create_dataset(k, data=np.array(v), compression='gzip')
